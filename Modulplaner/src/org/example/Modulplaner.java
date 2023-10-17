bd9c3859b0c972576503bea4d6392b490bfbe19d
the-one-and-only-niels <Niels.Fricke@t-online.de>
2023-10-17 13:44:44 +0200

    weiter

--- Modulplaner/src/org/example/Modulplaner.java
+++ Modulplaner/src/org/example/Modulplaner.java
@@ -14,44 +14,39 @@ import org.example.MenuBar.MenuBar;
  * @author Niels Fricke <Niels.Fricke@t-online.de>
  */
 public class Modulplaner extends JFrame {
-    
+
     /**
      * @param args the command line arguments
      */
-    
     //public static Modulplaner app;
-    
     private ArrayList<Modul> module = new ArrayList();
-    
-    public Modulplaner(){
+
+    public Modulplaner() {
         setTitle("Planer");
         addWindowListener(new WindowEventListener());
         setSize(500, 300);
         setLocationRelativeTo(null);
-        
+
         setJMenuBar(new MenuBar("modulplaner"));
-        
-        module.add(new Modul("P3","Montag", 8,9));
+
+        module.add(new Modul("P3", "Montag", 8, 9));
         System.out.println(module.get(0).getName());
-        
+
         setLayout(new BorderLayout());
 
         setVisible(true);
     }
 
-    
     public static void main(String[] args) {
-        
+
         /*
         if (System.getProperty("os.name") != null && System.getProperty("os.name").toLowerCase().indexOf("mac") != -1) {
             //System.getProperties().put("apple.laf.useScreenMenuBar", "true");
             System.setProperty("apple.laf.useScreenMenuBar", "true");
         }*/
-        
         //app = new Modulplaner();
         new Modulplaner();
-        
+
         //new About();
-        
     }
 }
