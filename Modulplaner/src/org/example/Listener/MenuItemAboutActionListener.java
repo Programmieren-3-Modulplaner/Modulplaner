0bfb154d7813a658e75a686d2a0b3557f4c73f81
the-one-and-only-niels <Niels.Fricke@t-online.de>
2023-10-17 13:44:04 +0200

    weiter

--- Modulplaner/src/org/example/Listener/MenuItemAboutActionListener.java
+++ Modulplaner/src/org/example/Listener/MenuItemAboutActionListener.java
@@ -4,19 +4,9 @@
  */
 package org.example.Listener;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.border.LineBorder;
 import org.example.About;
-import org.example.MenuBar.MenuBar;
 
 /**
  * @author Niels Fricke <Niels.Fricke@t-online.de>
@@ -26,7 +16,6 @@ public class MenuItemAboutActionListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         System.out.println("About öffnen");
-        
-        About about = new About();
+        new About();
     }
 }
