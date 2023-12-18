a39a5ce071cc16a7860e1c79b9e2648357d2faee
TJMaas24 <tjark.maasberg@hotmail.com>
2023-12-18 19:40:38 +0100

    Add new languages for the help
    Es wurden neue Sprachen für die Hilfe hinzugefügt.
    Englisch und Französisch kann nun nach Sprachauswahl angezeigt werden.

--- Modulplaner/src/org/example/Hilfe.java
+++ Modulplaner/src/org/example/Hilfe.java
@@ -13,16 +13,37 @@ public class Hilfe extends JFrame {
     public Hilfe(Modulplaner p) {
         this.parent = p;
         
-        File pdfPfad = new File("docs/Hilfe_Modulplaner.pdf");
-        try {
-            Desktop desktop = Desktop.getDesktop();
-            if (desktop != null && desktop.isSupported(Desktop.Action.OPEN)) {
-	        //Öffnet den Dateipfad mit dem Standartprogramm des Systems
-                desktop.open(pdfPfad); 
-            } 
-        }catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
+        //Sprachecode für die Unterscheidung der Hilfesprache
+        String languageCode = parent.getLanguageString();
+        System.out.println(languageCode);
+        String pdfFileName="";
+
+    switch (languageCode) {
+        case "en":
+            pdfFileName = "Hilfe_Modulplaner_EN.pdf";
+            break;
+        case "de":
+            pdfFileName = "Hilfe_Modulplaner_DE.pdf";
+            break;
+        case "fr":
+            pdfFileName = "Hilfe_Modulplaner_FR.pdf";
+            break;
+        default: 
+            pdfFileName = "Hilfe_Modulplaner_EN.pdf";
+            break;
+    }
+
+    File pdfPfad = new File("docs/" + pdfFileName);
+    try {
+        Desktop desktop = Desktop.getDesktop();
+        if (desktop != null && desktop.isSupported(Desktop.Action.OPEN)) {
+            //Öffnet den Dateipfad mit dem Standartprogramm des Systems
+            desktop.open(pdfPfad); 
+        } 
+    }catch (IOException e) {
+        System.out.println(e.getMessage());
+    }
         
     }
 }
+
