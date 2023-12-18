92574b7e57426bcb80150eb04c26f61b2864c033
TJMaas24 <tjark.maasberg@hotmail.com>
2023-12-18 20:00:39 +0100

    Rework help class

--- Modulplaner/src/org/example/Hilfe.java
+++ Modulplaner/src/org/example/Hilfe.java
@@ -4,6 +4,7 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 
 public class Hilfe extends JFrame {
@@ -15,34 +16,42 @@ public class Hilfe extends JFrame {
         
         //Sprachecode für die Unterscheidung der Hilfesprache
         String languageCode = parent.getLanguageString();
-        System.out.println(languageCode);
+        
+        //Platzhalter für den Dateipfad
         String pdfFileName="";
 
-    switch (languageCode) {
-        case "en":
-            pdfFileName = "Hilfe_Modulplaner_EN.pdf";
-            break;
-        case "de":
-            pdfFileName = "Hilfe_Modulplaner_DE.pdf";
-            break;
-        case "fr":
-            pdfFileName = "Hilfe_Modulplaner_FR.pdf";
-            break;
-        default: 
-            pdfFileName = "Hilfe_Modulplaner_EN.pdf";
-            break;
-    }
-
-    File pdfPfad = new File("docs/" + pdfFileName);
-    try {
-        Desktop desktop = Desktop.getDesktop();
-        if (desktop != null && desktop.isSupported(Desktop.Action.OPEN)) {
-            //Öffnet den Dateipfad mit dem Standartprogramm des Systems
-            desktop.open(pdfPfad); 
-        } 
-    }catch (IOException e) {
-        System.out.println(e.getMessage());
-    }
+        switch (languageCode) {
+            case "en":
+                pdfFileName = "Hilfe_Modulplaner_EN.pdf";
+                break;
+            case "de":
+                pdfFileName = "Hilfe_Modulplaner_DE.pdf";
+                break;
+            case "fr":
+                pdfFileName = "Hilfe_Modulplaner_FR.pdf";
+                break;
+            default: 
+                pdfFileName = "Hilfe_Modulplaner_EN.pdf";
+                break;
+        }
+    
+        //Zusammensetzung des Dateipfades
+        File pdfPfad = new File("docs/" + pdfFileName);
+        try {
+            //Initzialisierung des Desktop -> so können Actionen des Desktops des Benutzers verwendet werden
+            Desktop desktop = Desktop.getDesktop();
+            
+            //Prüfung ob das System die Methode unterstützt
+            if (desktop != null && desktop.isSupported(Desktop.Action.OPEN)) {
+                
+                //Öffnet den Dateipfad mit dem Standartprogramm des Systems
+                desktop.open(pdfPfad); 
+            } else {
+                JOptionPane.showMessageDialog(parent, "Datei kann nicht geöffnet werden");
+            }
+        }catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
         
     }
 }
